c1b3fb0a4f073b0019ba3633d02a716b3be3242b
Robert Cortez <Nuhuhfoo@gmail.com>
2019-04-12 21:37:48 -0700

    update position of appBar and Tab text

--- src/components/Buttons/Buttons.js
+++ src/components/Buttons/Buttons.js
@@ -30,6 +30,12 @@ const styles = theme => ({
     paddingTop: '25px',
     color: '#FFFFFF',
   },
+  text: {
+    color: '#e6e6e6',
+    width: '90%',
+    margin: 'auto',
+    marginTop: '10px',
+  },
 });
 
 class ScrollableTabsButtonForce extends React.Component {
@@ -65,7 +71,20 @@ class ScrollableTabsButtonForce extends React.Component {
             <Tab label="Surgery" />
           </Tabs>
         </AppBar>
-        {value === 0 && <TabContainer>Item One</TabContainer>}
+        {value === 0 && <TabContainer><p className={classes.text}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's 
+            standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has 
+            survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of 
+            Letraset sheets containing Lorem Ipsum passages, 
+            and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            Why do we use it?</p>
+            <br></br>
+            <p className={classes.text}>
+            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. 
+            The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, 
+            content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum 
+            as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, 
+            sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+            <br></br></TabContainer>}
         {value === 1 && <TabContainer>Item Two</TabContainer>}
         {value === 2 && <TabContainer>Item Three</TabContainer>}
         {value === 3 && <TabContainer>Item Four</TabContainer>}
