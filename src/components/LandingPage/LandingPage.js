2a567b67ca76f9b44fc1b51db2fcb04ef7acaa3b
Robert Cortez <Nuhuhfoo@gmail.com>
2019-05-03 21:20:06 -0700

    fixed landing page layout

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,15 +1,22 @@
 import { Component } from "react";
 import React from 'react';
 import './LandingPage.css';
-import logo from '../../images/logo.png';
+import logo from '../../images/logo.jpg';
+
 
 class LandingPage extends Component {
     render() {
         return(
             <div className="intro">
-                <img className="image rotate-center" src={logo} alt="Logo" />
-                <h1 className="title">BROADWAY PET CLINIC</h1>
-                <p className="addy">1058 E. Broadway Long Beach, CA 90802<br />562.432.1413</p>
+                
+                    <img className="image bounce-top" src={logo} alt="Logo" />
+                
+                <div>
+                    <p className="title text-focus-in">BROADWAY PET CLINIC 
+                        <br></br> 
+                        <p className="addy">1058 E. Broadway Long Beach, CA 90802<br />562.432.1413</p>
+                    </p>
+                </div>
             </div>
         )
     }
