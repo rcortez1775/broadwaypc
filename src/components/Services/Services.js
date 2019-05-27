f1d77f79eea773624653626bafef96cf48fc03ca
Robert Cortez <Nuhuhfoo@gmail.com>
2019-05-27 15:54:52 -0700

    added some content to services page

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -1,6 +1,7 @@
 import { Component } from "react";
 import React from 'react';
 import './Services.css';
+import next from '../../images/next.png';
 
 
 class Services extends Component {
@@ -9,6 +10,27 @@ class Services extends Component {
             <div>
                 <div className="servicesContainer">
                     <p className="services">OUR SERVICES</p>
+                </div>
+
+                <div className="pricesCont">
+                    <p className="serviceTitle"><img className="next" src={next} alt="next" />Price list of Services</p>
+                    <ul>
+                        <li>Physical exam - $45</li>
+                        <li>In-house blood work - $149</li>
+                        <li>Nail Trim - $20</li>
+                        <li>Anal Gland Expression - $15</li>
+                        <li>Microchip - $30</li>
+                        <li>Ultrasound - $350</li>
+                        <li>Radiographs - $100 for first view. $45 for each additional view</li>
+                        <li>Dental - starts at $199(not including extractions/special circumstances)</li>
+                        <p className="listItem">Vaccines:</p>
+                        <li>DA2PP -$19</li>
+                        <li>Corona - $19</li>
+                        <li>Bordetella Booster - $19</li>
+                        <li>FVRCP Booster - $19</li>
+                        <li>FELV Booster - $22</li>
+                        <li>Rabies Vaccine - $18</li>
+                    </ul>
                 </div>
             </div>
         )
